69cabf3cd3efe1d1a806355d34f3f402b5a669a9
Awadhesh Kumar <121618523+awadhesh94@users.noreply.github.com>
2023-05-08 16:58:45 +0530

    added mock data from cards (#4)

--- mocks/card-mock.ts
+++ mocks/card-mock.ts
@@ -2,19 +2,59 @@ import { CardProps } from '../types/card';
 
 export const CardMock: CardProps[] = [
   {
-    date: 'Apr 1, 2023',
-    like: '2K',
-    title: 'planetfall.io',
+    date: '2023-05-08',
+    like: '1.2K',
+    title: 'Lorem ipsum dolor sit amet',
     description:
-      "I'm building a SAAS providing global latency monitoring for your APIs and websites from edge locations around the world. Have you ever wondered how fast your API is in any part of the world? Planetfall allows you to find out and monitor it continuously.",
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat odio sit amet quam tincidunt, eu mollis nibh blandit. Sed suscipit imperdiet metus, vitae commodo lacus euismod at. Praesent non ultrices est. Nulla sodales ut nisl quis tristique.',
     slug: '/',
   },
   {
-    date: 'July 18, 2023',
-    like: '2K',
-    title: '@awadhesh/ui-gem',
+    date: '2023-05-07',
+    like: '2.8K',
+    title: 'Duis aute irure dolor in reprehenderit',
     description:
-      'A minimal library for access control. It is designed to be used together with opaque access tokens by providing a simple interface to define roles with different access permissions and verifying requests to resources.',
+      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.',
+    slug: '/',
+  },
+  {
+    date: '2023-05-06',
+    like: '3.5K',
+    title: 'Sed ut perspiciatis unde omnis',
+    description:
+      'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.',
+    slug: '/',
+  },
+  {
+    date: '2023-05-05',
+    like: '4.1K',
+    title: 'Nemo enim ipsam voluptatem quia',
+    description:
+      'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.',
+    slug: '/',
+  },
+  {
+    date: '2023-05-04',
+    like: '2.3K',
+    title: 'Neque porro quisquam est qui dolorem',
+    description:
+      'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
+    slug: '/',
+  },
+  {
+    date: '2023-05-03',
+    like: '5.6K',
+    title: 'Lorem ipsum dolor sit amet',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat odio sit amet quam tincidunt, eu mollis nibh blandit. Sed suscipit imperdiet metus, vitae commodo lacus euismod at.',
+    slug: '/',
+  },
+  {
+    date: '2023-05-02',
+    like: '1.8K',
+    title: 'Duis aute irure dolor in reprehenderit',
+    description:
+      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.',
     slug: '/',
   },
 ];
