f568f35a6683848b777f549be56d3c1c1de3d525
awadhesh94 <awadhesh31st@outlook.com>
2023-05-08 21:21:42 +0530

    updated content for home page

--- mocks/home-mock.ts
+++ mocks/home-mock.ts
@@ -4,5 +4,5 @@ import { NavigationMock } from './navigation-mock';
 export const HomeMock: HomeProps = {
   navigationsProps: NavigationMock,
   pageTitle: `hi, i'm awadhesh`,
-  pageDescription: `Software developer at Omnie where i build and test projects. I'm passionate about frontend development and have created courses on React, Next.js, and web development.`,
+  pageDescription: `As a software developer with 3 years of experience in the IT industry, my passion lies in front-end development. I have created courses on React, Next.js, and web development to share my knowledge with others. I am constantly learning and adapting to new technologies to deliver high-quality solutions.`,
 };
